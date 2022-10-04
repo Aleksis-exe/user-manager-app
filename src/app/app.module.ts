17ef4f402932f65ef79fc4cd2e2c4ecbcd00d7de
Alexey <aleksis.exe@yandex.ru>
2022-10-04 15:49:06 +0500

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component'
+import { StoreModule } from '@ngrx/store'
+import { MainLayoutComponent } from './layouts/main-layout/components/main-layout/main-layout.component'
+import { UsersPageComponent } from './pages/users-page/components/users-page/users-page.component'
+import { AppRoutingModule } from './app-routing.module'
+import { HeaderComponent } from './layouts/main-layout/components/header/header.component'
+import { AlertModule } from './modules/alert/alert.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    UsersPageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}, {}),
+    AppRoutingModule,
+    AlertModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
