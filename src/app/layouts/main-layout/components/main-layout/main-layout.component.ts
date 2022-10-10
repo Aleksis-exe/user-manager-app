d44f8d8d65fb7febba80c07ba1d98dbe6e441e28
Alexey <aleksis.exe@yandex.ru>
2022-10-10 11:44:55 +0500

    add main-layout

--- src/app/layouts/main-layout/components/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/components/main-layout/main-layout.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { select, Store } from '@ngrx/store'
 import setup from 'package.json'
+import { Observable } from 'rxjs'
+import { ITicket } from 'src/app/context/authorization/interfaces/ticket.interface'
+import * as authorizationSelect from 'src/app/context/authorization/authorization.select'
 
 @Component({
   selector: 'main-layout',
@@ -7,13 +11,29 @@ import setup from 'package.json'
   styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
+  ticket$: Observable<ITicket | null> | undefined
+
   version: string = ''
   author: string = ''
 
-  constructor() {}
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
     this.version = setup.version.toString()
     this.author = setup.author.toString()
+    this.ticket$ = this.store.pipe(select(authorizationSelect.ticketSelector))
+  }
+
+  getUserName(t: ITicket | null): string {
+    if (t !== null) {
+      if (t.fullName === ' ') return t.userName
+      return t.fullName
+    }
+    return 'userName'
+  }
+
+  getIcon(t: ITicket | null): string {
+    if (t !== null) return '/icon/' + t.userName
+    return '/icon'
   }
 }
